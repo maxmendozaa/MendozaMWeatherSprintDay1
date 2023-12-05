e704ef0354fb1318faf85cf9cb06d23debb0053c
maxmendozaa <148285732+maxmendozaa@users.noreply.github.com>
2023-12-05 10:46:51 -0800

    firstcommit

---
+++ scripts/apps.js
@@ -0,0 +1,16 @@
+// let dataHere = document.getElementById("dataHere");
+// let adviceBtn = document.getElementById("adviceBtn");
+
+// adviceBtn.addEventListener('click', function(e) {
+//     // alert("I work!");
+//     GetData();
+// });
+
+// function GetData(){
+//     fetch("https://api.adviceslip.com/advice").then(
+//         response => response.json()
+//     ).then(
+//         // data => console.log(data)
+//         data => dataHere.innerText = data.slip.advice  
+//     )
+// }
