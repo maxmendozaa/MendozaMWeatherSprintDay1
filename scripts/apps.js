8a968913d1bb567f8abda4ca30252c8e6a5d2922
maxmendozaa <148285732+maxmendozaa@users.noreply.github.com>
2023-12-06 10:33:16 -0800

    moredesign

--- scripts/apps.js
+++ scripts/apps.js
@@ -7,7 +7,7 @@
 // });
 
 // function GetData(){
-//     fetch("https://api.adviceslip.com/advice").then(
+//     fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7").then(
 //         response => response.json()
 //     ).then(
 //         // data => console.log(data)
