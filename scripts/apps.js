9fa3851b06003255560fd5e240fdf64e88184a2a
maxmendozaa <148285732+maxmendozaa@users.noreply.github.com>
2023-12-08 15:26:46 -0800

    final edits

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,13 +1,30 @@
 
- async function currentDate(){
+ async function curTemp(){
     // this function is grabbing data from this url/api which is weather long and lat
     let promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7`)
     //this line creates new data and the await is tells the data to wait while the promise holds the api data. the json is trying the promise into a object
     let data = await promise.json();
-    console.log(data);
+    let curTemp = data.main.temp_max + `F` + `/` + data.main.temp_min + `F`
+    console.log(data.main.temp);
+    document.getElementById(`curTemp`).innerText = curTemp;
 };
 
-currentDate();
+curTemp();
 
+async function currentWeather(){
+    let promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7`)
+    let data = await promise.json();
+    let currentWeather = data.main.temp + `F°`
+    console.log(data.main.temp);
+    document.getElementById(`currentWeather`).innerText = currentWeather;
+}
 
-// fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7`);
+currentWeather();
+
+// async function currentTime(){
+//     const d = new Date();
+//     let time = d.getTime();
+//     document.getElementById("currentTime").innerHTML = currentTime;
+// };
+
+// currentTime();
