93e62efc94ed863fcb6e3db96fce1911b7d9887e
maxmendozaa <148285732+maxmendozaa@users.noreply.github.com>
2023-12-08 10:58:55 -0800

    final design changes

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,16 +1,13 @@
-// let dataHere = document.getElementById("dataHere");
-// let adviceBtn = document.getElementById("adviceBtn");
 
-// adviceBtn.addEventListener('click', function(e) {
-//     // alert("I work!");
-//     GetData();
-// });
+ async function currentDate(){
+    // this function is grabbing data from this url/api which is weather long and lat
+    let promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7`)
+    //this line creates new data and the await is tells the data to wait while the promise holds the api data. the json is trying the promise into a object
+    let data = await promise.json();
+    console.log(data);
+};
 
-// function GetData(){
-//     fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7").then(
-//         response => response.json()
-//     ).then(
-//         // data => console.log(data)
-//         data => dataHere.innerText = data.slip.advice  
-//     )
-// }
+currentDate();
+
+
+// fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=121.2908&appid=d899f449b82cfcc448b390ab82b924e7`);
